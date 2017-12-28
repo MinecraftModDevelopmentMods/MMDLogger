40112f421186f32636e7de4b3f55f144eadfdc5a
SkyBlade1978 <johnbraham1978@outlook.com>
2017-12-28 00:53:51 +0000

    Now links into the items related to the oredict entries and logs

--- src/main/java/com/mcmoddev/mmdlogger/MMDLogger.java
+++ src/main/java/com/mcmoddev/mmdlogger/MMDLogger.java
@@ -1,14 +1,20 @@
 package com.mcmoddev.mmdlogger;
 
 import net.minecraftforge.oredict.OreDictionary;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = MMDLogger.MODID, version = MMDLogger.VERSION)
 public class MMDLogger
@@ -36,10 +42,25 @@ public class MMDLogger
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-    	if (loggingOn)
-    		for (String oreName : OreDictionary.getOreNames()) 
-    			logger.info("Ore Dictionary Entry: %s, %s", oreName, 
-    					OreDictionary.getOreID(oreName));
-		
+    	if (loggingOn) {	
+    		List<ItemStack> items;
+    		int oreID;
+    		
+    		for (String oreName : OreDictionary.getOreNames()) {    			
+    			oreID = OreDictionary.getOreID(oreName); 
+    			
+    			items = OreDictionary.getOres(oreName);
+    			
+    			for (ItemStack itemStack : items) {
+    				logger.info("Ore Dictionary Entry: Ore Name: %s, Ore ID: %s, Unlocalised Name: %s", oreName, oreID, itemStack.getItem().getUnlocalizedName());	
+				}
+    		} 	
+    	}
     }
+    
+    @SideOnly(Side.CLIENT)
+	public static void addToolTip(String name, java.util.List<String> tooltipList) {
+		tooltipList.add(I18n.format(name, 4));
+		
+	}
 }
