353faf88443f980ed61040c57ad0ce7f5eb992e3
SkyBlade1978 <johnbraham1978@outlook.com>
2017-12-29 18:11:29 +0000

    a few tidy ups

--- src/main/java/com/mcmoddev/mmdlogger/MMDLogger.java
+++ src/main/java/com/mcmoddev/mmdlogger/MMDLogger.java
@@ -6,11 +6,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configurator;
-
-import net.minecraft.block.Block;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
@@ -18,17 +13,17 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
+/**
+ * @author SkyBlade1978
+ *
+ */
 @Mod(modid = MMDLogger.MODID, version = MMDLogger.VERSION)
-public class MMDLogger
-{
+public class MMDLogger {
     public static final String MODID = "mmdlogger";
     public static final String VERSION = "1.0";
     
 	private Logger logger;
-	private LoggerContext loggerContext;
 	private boolean loggingOn = false;
 	
 	@EventHandler
@@ -66,14 +61,6 @@ public class MMDLogger
     				logger.info("Ore Dictionary Entry: Ore Name: %s, Ore ID: %s, Unlocalised Name: %s, Block ID: %s, Registry Name: %s", oreName, oreID, item.getUnlocalizedName(), Item.getIdFromItem(item), item.getRegistryName());	
 				}
     		}
-    		
-    		Configurator.shutdown(loggerContext);
     	}
     }
-    
-    @SideOnly(Side.CLIENT)
-	public static void addToolTip(String name, java.util.List<String> tooltipList) {
-		tooltipList.add(I18n.format(name, 4));
-		
-	}
 }
