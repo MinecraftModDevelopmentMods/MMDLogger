29ea6d86ffc8e65322b9802c74e23d840e95d309
faceofcat <candreix@gmail.com>
2018-02-27 14:40:10 +0200

    updated gradle to 4.4.1; added config class to get gui and such; badly fixes #7: Add NBT information to tooltip;

---
+++ src/main/java/com/mcmoddev/mmdlogger/MMDLoggerConfig.java
@@ -0,0 +1,36 @@
+package com.mcmoddev.mmdlogger;
+
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Config(modid = MMDLogger.MODID)
+public class MMDLoggerConfig {
+    public static Options options = new Options();
+
+    public static class Options {
+        @Config.Comment("If true, then ore dict names and corresponding id's are logged.")
+        public boolean loggingOn = false;
+
+        @Config.Comment("If true, then ore dict names are displayed in tooltips.")
+        public boolean oreDictTooltipsOn = false;
+
+        @Config.Comment("If true, then item stack's nbt is displayed in tooltips.")
+        public boolean nbtTooltipsOn = false;
+    }
+
+    @Mod.EventBusSubscriber(modid = MMDLogger.MODID)
+    public static class ConfigSyncHandler
+    {
+        @SubscribeEvent
+        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event)
+        {
+            if(event.getModID().equals(MMDLogger.MODID))
+            {
+                ConfigManager.sync(MMDLogger.MODID, Config.Type.INSTANCE);
+            }
+        }
+    }
+}
--- src/main/java/com/mcmoddev/mmdlogger/MMDLoggerEventBusSubscriber.java
+++ src/main/java/com/mcmoddev/mmdlogger/MMDLoggerEventBusSubscriber.java
@@ -1,9 +1,9 @@
 package com.mcmoddev.mmdlogger;
 
 import java.util.List;
-
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,29 +11,35 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = MMDLogger.MODID)
 public class MMDLoggerEventBusSubscriber {
-
 	@SubscribeEvent
-    public void onToolTip(ItemTooltipEvent event)
+    public static void onToolTip(ItemTooltipEvent event)
     {
-		if (MMDLogger.tooltipsOn) {
-			ItemStack stack = event.getItemStack();
-			Item item = stack.getItem();
-			
-			if (item == null)
-				return;
-			
-	    	String blockName = item.getRegistryName().getResourceDomain() + ":" + item.getRegistryName().getResourcePath();
-	    	int meta = item.getMetadata(stack);
-	    	
-	    	
-	    	String oreName = MMDLogger.ItemToOreDictMap.get(blockName + ":" + meta);
-	    	
-	    	List<String> tooltips = event.getToolTip();
-		    
-	    	if (oreName != null) {
-	    		tooltips.add(TextFormatting.BLUE.toString() + TextFormatting.ITALIC.toString() + oreName);
-	    	}
-    	
+        List<String> tooltips = event.getToolTip();
+
+		if (MMDLoggerConfig.options.oreDictTooltipsOn) {
+            ItemStack stack = event.getItemStack();
+            Item item = stack.getItem();
+
+            if (item == null)
+                return;
+
+            // ore dictionary
+            String blockName = item.getRegistryName().getResourceDomain() + ":" + item.getRegistryName().getResourcePath();
+            int meta = item.getMetadata(stack);
+
+            String oreName = MMDLogger.ItemToOreDictMap.get(blockName + ":" + meta);
+
+            if (oreName != null) {
+                tooltips.add(TextFormatting.BLUE.toString() + TextFormatting.ITALIC.toString() + oreName);
+            }
+        }
+
+        if (MMDLoggerConfig.options.nbtTooltipsOn) {
+	    	// nbt
+			NBTTagCompound stackNbt = event.getItemStack().getTagCompound();
+	    	if ((stackNbt != null) && !stackNbt.hasNoTags()) {
+	    		tooltips.add(TextFormatting.GRAY + "NBT: " + stackNbt.toString());
+			}
 		}
     }
 }
