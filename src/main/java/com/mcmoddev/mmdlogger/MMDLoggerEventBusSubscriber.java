6d1fc67cf7f4b8c1f2061569628154b5d658ec54
faceofcat <candreix@gmail.com>
2018-02-28 07:14:59 +0200

    item block tooltips

--- src/main/java/com/mcmoddev/mmdlogger/MMDLoggerEventBusSubscriber.java
+++ src/main/java/com/mcmoddev/mmdlogger/MMDLoggerEventBusSubscriber.java
@@ -1,7 +1,12 @@
 package com.mcmoddev.mmdlogger;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.text.TextFormatting;
@@ -15,15 +20,21 @@ public class MMDLoggerEventBusSubscriber {
     public static void onToolTip(ItemTooltipEvent event)
     {
         List<String> tooltips = event.getToolTip();
+        ItemStack stack = event.getItemStack();
+        //noinspection ConstantConditions
+        if ((stack == null) || stack.isEmpty()) {
+            return;
+        }
+
+        // TODO: consider testing for advanced tooltips being turned on also
+        // if (!event.getFlags().isAdvanced()) return;
 
 		if (MMDLoggerConfig.options.oreDictTooltipsOn) {
-            ItemStack stack = event.getItemStack();
             Item item = stack.getItem();
-
+            //noinspection ConstantConditions
             if (item == null)
                 return;
 
-            // ore dictionary
             String blockName = item.getRegistryName().getResourceDomain() + ":" + item.getRegistryName().getResourcePath();
             int meta = item.getMetadata(stack);
 
@@ -35,11 +46,34 @@ public class MMDLoggerEventBusSubscriber {
         }
 
         if (MMDLoggerConfig.options.nbtTooltipsOn) {
-	    	// nbt
 			NBTTagCompound stackNbt = event.getItemStack().getTagCompound();
 	    	if ((stackNbt != null) && !stackNbt.hasNoTags()) {
 	    		tooltips.add(TextFormatting.GRAY + "NBT: " + stackNbt.toString());
 			}
 		}
+
+		if (MMDLoggerConfig.options.blockTooltipsOn && (stack.getItem() instanceof ItemBlock)) {
+            Block block = ((ItemBlock)stack.getItem()).getBlock();
+            IBlockState state = block.getDefaultState();
+
+            tooltips.add(TextFormatting.GRAY + "Occlusion: " + state.getAmbientOcclusionLightValue());
+            tooltips.add(TextFormatting.DARK_GRAY + "Light: " + state.getLightValue());
+
+            try {
+                Field hardness = Block.class.getDeclaredField("blockHardness");
+                hardness.setAccessible(true);
+                tooltips.add(TextFormatting.GRAY + "Hardness: " + hardness.get(block));
+            } catch (NoSuchFieldException | IllegalAccessException ignored) {}
+
+            try {
+                Field resistance = Block.class.getDeclaredField("blockResistance");
+                resistance.setAccessible(true);
+                tooltips.add(TextFormatting.GRAY + "Resistance: " + resistance.get(block));
+            } catch (NoSuchFieldException | IllegalAccessException ignored) {}
+
+            tooltips.add(TextFormatting.DARK_GRAY + "Flammability: " + Blocks.FIRE.getFlammability(block));
+
+            tooltips.add(TextFormatting.GRAY + "Max stack size: " + stack.getMaxStackSize());
+        }
     }
 }
